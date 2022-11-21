c274436d4139c2e9945654ad23ac71cb0d329928
André Licks <andrelicks@gmail.com>
2022-11-21 15:22:14 -0400

    Correção do form

--- src/pages/PageSinal.js
+++ src/pages/PageSinal.js
@@ -1,5 +1,40 @@
+import {Row, Col, Button} from "react-bootstrap";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+
 function PageSinal() {
-  return <div> PageSinal</div>;
+	const navigate = useNavigate();
+	const { termoId } = useParams();
+
+	async function handleDelete(e) {
+		await axios.delete(`https://ironrest.cyclic.app/wikilibras/${termoId}`);
+		navigate("/");
+	}
+
+	return (
+		<div>
+			<Row>
+				<Col>
+					<Link to={"/"}>
+						<Button variant="error" size="lg" className="mb-3" onClick={handleDelete}>
+							Deletar
+						</Button>
+					</Link>
+				</Col>
+				<Col>
+					<Button
+						variant="Primary"
+						size="lg"
+						className="mb-3"
+						
+					>
+						Editar
+					</Button>
+				</Col>
+			</Row>
+		</div>
+	);
 }
 
 export default PageSinal;
