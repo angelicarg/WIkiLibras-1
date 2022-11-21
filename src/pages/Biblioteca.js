d0c606941de7adeaaf094eefa71d7bd7bdd5689a
André Licks <andrelicks@gmail.com>
2022-11-21 16:49:45 -0400

    Botão de deletar funcionando

--- src/pages/Biblioteca.js
+++ src/pages/Biblioteca.js
@@ -1,52 +1,54 @@
-import { Container, Button, Card } from 'react-bootstrap';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Container, Button, Card } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Biblioteca() {
-  const [termos, setTermos] = useState([]);
+	const [termos, setTermos] = useState([]);
 
-  useEffect(() => {
-    async function pegarTermos() {
-      const response = await axios.get(
-        'https://ironrest.cyclic.app/wikilibras/'
-      );
+	useEffect(() => {
+		async function pegarTermos() {
+			const response = await axios.get(
+				"https://ironrest.cyclic.app/wikilibras/"
+			);
 
-      setTermos(response.data);
-    }
+			setTermos(response.data);
+		}
 
-    pegarTermos();
-  }, []);
-  console.log(termos);
-  return (
-    <div>
-      <Container>
-        {termos.map(termo => {
-          let url = termo.linkTermo.replace('watch?v=', 'embed/');
+		pegarTermos();
+	}, []);
 
-          return (
-            <Card>
-              <iframe
-                width="560"
-                height="315"
-                src={url}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-              <Card.Body>
-                <Card.Text>{termo.name}</Card.Text>
-              </Card.Body>
-              <Link to={`/paginadosinal/${termo._id}`}>
-                <Button variant="outline-primary">Saiba Mais!</Button>
-              </Link>
-            </Card>
-          );
-        })}
-      </Container>
-    </div>
-  );
+	return (
+		<div>
+			<Container>
+				{termos.map((termo) => {
+					let url = termo.linkTermo.replace("watch?v=", "embed/");
+
+					return (
+						<div key={termo._id}>
+							<Card>
+								<iframe
+									width="560"
+									height="315"
+									src={url}
+									title="YouTube video player"
+									frameBorder="0"
+									allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+									allowFullScreen
+								></iframe>
+								<Card.Body>
+									<Card.Text>{termo.termo}</Card.Text>
+								</Card.Body>
+								<Link to={`/PageSinal/${termo._id}`}>
+									<Button variant="outline-primary">Saiba Mais!</Button>
+								</Link>
+							</Card>
+						</div>
+					);
+				})}
+			</Container>
+		</div>
+	);
 }
 
 export default Biblioteca;
